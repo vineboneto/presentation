2658c7489349d119c258f7985cb94e373995099c
vineboneto <vineboneto@gmail.com>
2022-06-30 20:43:38 -0300

    refactor: change bar color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html data-theme="dark">
-      <Head />
+      <Head>
+        <meta name="theme-color" content="#2a303b" />
+      </Head>
       <body>
         <Main />
         <NextScript />
