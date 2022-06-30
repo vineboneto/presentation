15e5a028a06b285c463a064eb9bbb42d4e4545fd
vineboneto <vineboneto@gmail.com>
2022-06-30 03:32:22 -0300

    about page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,16 +3,16 @@ export function Header() {
     <div className="navbar fixed">
       <div className="container mx-auto">
         <div className="flex justify-between items-center w-full py-2">
-          <h3 className="bg-transparent clip-text text-[2rem]">Vine Boneto</h3>
+          <h3 className="bg-transparent clip-text text-2xl text-base-content">Vine Boneto</h3>
           <div className="space-x-4">
             <a href="#">
-              <span className="clip-text text-[1.5rem]">Home</span>
+              <span className="clip-text text-xl text-base-content">Home</span>
             </a>
             <a href="#">
-              <span className="clip-text text-[1.5rem]">Contato</span>
+              <span className="clip-text text-xl text-base-content">Contato</span>
             </a>
             <a href="#">
-              <span className="clip-text text-[1.5rem]">Portfólio</span>
+              <span className="clip-text text-xl text-base-content">Portfólio</span>
             </a>
           </div>
         </div>
