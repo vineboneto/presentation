2f7b43c519cf5350ba81bc121a777cdfb413adc5
vineboneto <vineboneto@gmail.com>
2022-07-02 05:51:06 -0300

    refactor: change backgournd color coder on change theme light

--- src/components/coder.tsx
+++ src/components/coder.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@/contexts'
 import { useScript } from '@/hooks'
 import { ScriptLine } from './script-line'
 
@@ -25,15 +26,24 @@ const velocityInMs = 50
 export function Coder() {
   const scriptWide = useScript({ fullScript: fullScriptsWide, velocityInMs })
   const scriptMobile = useScript({ fullScript: fullScriptsMobile, velocityInMs })
+  const { theme } = useTheme()
+
   return (
     <>
-      <div className="mockup-code w-[650px] z-10 rounded-xl hidden md:block">
+      <div
+        className={'mockup-code w-[650px] z-10 rounded-xl hidden md:block'.concat(
+          ' ',
+          theme === 'light' ? 'bg-slate-500' : ''
+        )}
+      >
         {scriptWide.scripts.map((script, idx, self) => (
           <ScriptLine key={idx} installLine={1} endLine={self.length - 1} idx={idx} script={script} />
         ))}
       </div>
 
-      <div className="mockup-code w-[350px] z-10 block md:hidden">
+      <div
+        className={'mockup-code w-[350px] z-10 block md:hidden'.concat(' ', theme === 'light' ? 'bg-slate-500' : '')}
+      >
         {scriptMobile.scripts.map((script, idx, self) => (
           <ScriptLine key={idx} installLine={1} endLine={self.length - 1} idx={idx} script={script} />
         ))}
